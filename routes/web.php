2c87289c00e10876dc22a5d130ac0aa7c42aaa3f
Munotasir2762 <muntasirpranto2762@gmail.com>
2024-08-19 00:02:47 +0600

    database & auth

--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,7 @@ Route::get('/', [FrontendController::class, 'index']);
 Route::get('/product/details', [FrontendController::class, 'productDetails']);
 Route::get('/view-cart', [FrontendController::class, 'viewCart']);
 Route::get('/checkout', [FrontendController::class, 'checkout']);
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
