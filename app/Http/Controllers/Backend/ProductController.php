5c0755b54beb01bcc89e120bf16c98f571a34c5a
Munotasir2762 <muntasirpranto2762@gmail.com>
2024-09-22 21:54:34 +0600

    Product upload is done

--- app/Http/Controllers/Backend/ProductController.php
+++ app/Http/Controllers/Backend/ProductController.php
@@ -5,7 +5,9 @@ namespace App\Http\Controllers\Backend;
 use App\Http\Controllers\Controller;
 use App\Models\Category;
 use App\Models\Color;
+use App\Models\GalleryImage;
 use App\Models\Product;
+use App\Models\Size;
 use App\Models\Subcategory;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
@@ -47,11 +49,35 @@ class ProductController extends Controller
 
         // Add Color...
         if(isset($request->color)){
-            foreach($request->color as $color){
+            foreach($request->color as $colorName){
                 $color = new Color();
                 $color->product_id = $product->id;
-                $color->color_name = $color;
+                $color->color_name = $colorName;
                 $color->save();
+            }
+        }
+
+        // Add Size...
+        if(isset($request->size)){
+            foreach($request->size as $sizeName){
+                $size = new Size();
+                $size->product_id = $product->id;
+                $size->size_name = $sizeName;
+                $size->save();
+            }
+        }
+
+        // Gallery Image....
+        if(isset($request->galleryImage)){
+            foreach($request->galleryImage as $image){
+                $galleryImage = new GalleryImage();
+                $galleryImage->product_id = $product->id;
+
+                $imageName = rand().'-gallery-'.'.'.$image->extension();
+                $image->move('backend/images/galleryImage/', $imageName);
+
+                $galleryImage->image = $imageName;
+                $galleryImage->save();
             }
         }
 
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -10,4 +10,30 @@ class Product extends Model
     use HasFactory;
 
     protected $guarded = [];
+
+    public function color ()
+    {
+        return $this->hasMany(Color::class, 'product_id', 'id');
+    }
+
+    public function size ()
+    {
+        return $this->hasMany(Size::class, 'product_id', 'id');
+    }
+
+    public function galleryImage ()
+    {
+        return $this->hasMany(GalleryImage::class, 'product_id', 'id');
+    }
+
+    public function category ()
+    {
+        return $this->belongsTo(Category::class, 'cat_id', 'id');
+    }
+
+    public function subCategory ()
+    {
+        return $this->belongsTo(Subcategory::class, 'sub_cat_id', 'id');
+    }
+
 }
